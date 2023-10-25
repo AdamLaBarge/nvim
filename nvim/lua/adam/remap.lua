53bc4b14006acb385f54be0749e266070a873547
AdamLaBarge <139151022+AdamLaBarge@users.noreply.github.com>
2023-10-24 20:53:58 -0500

    Add files via upload

---
+++ nvim/lua/adam/remap.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.keymap.set ("n", "<leader>ee", vim.cmd.Ex)
+vim.keymap.set ("n", "<leader>ww", vim.cmd.Neoformat)
+vim.keymap.set ("n", "<leader>ss", vim.cmd.w)
+vim.keymap.set ("n", "<leader>vbg", vim.cmd.VimBeGood)
+vim.keymap.set ("i", "jk", "<ESC>")
+vim.keymap.set ("n", "<leader>tt", vim.cmd.NvimTreeToggle)
